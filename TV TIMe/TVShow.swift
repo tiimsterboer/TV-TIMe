7ebb55f3caa630a955770c1b42c45fc13b87eeaa
Tim Beemsterboer <tim.beemsterboer@gmail.com>
2018-03-01 13:33:07 -0800

    first commit

---
+++ TV TIMe/TVShow.swift
@@ -0,0 +1,85 @@
+//
+//  TVShow.swift
+//  TV TIMe
+//
+//  Created by Tim Beemsterboer on 2/26/18.
+//  Copyright © 2018 Tim Beemsterboer. All rights reserved.
+//
+
+import Foundation
+import os.log
+
+class TVShow: NSObject, NSCoding {
+    
+    //properties
+    var id: Int32
+    var name: String
+    var type: String
+    var language: String
+    var summary: String
+    var genres: [String]
+    
+    //Types
+    struct PropertyKey {
+        static let id = "id"
+        static let name = "name"
+        static let type = "type"
+        static let language = "language"
+        static let summary = "summary"
+        static let genres = "genres"
+    }
+    
+    //Archiving Paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("shows")
+    
+    //initializer
+    init?(json: [String: Any]) {
+        guard
+            let id = json["id"] as? Int32,
+            let name = json["name"] as? String
+            else {
+                return nil
+        }
+        self.id = id
+        self.name = name
+        self.type = (json["type"] as? String)!
+        self.language = (json["language"] as? String)!
+        self.summary = (json["summary"] as? String)!
+        self.genres = (json["genres"] as? [String])!
+    }
+    
+    //Initialization
+    init?(id: Int32, name: String, type: String, language: String, summary: String, genres: [String]) {
+        self.id = id
+        self.name = name
+        self.type = type
+        self.language = language
+        self.summary = summary
+        self.genres = genres
+        super.init()
+        
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(id, forKey: PropertyKey.id)
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(type, forKey: PropertyKey.type)
+        aCoder.encode(language, forKey: PropertyKey.language)
+        aCoder.encode(summary, forKey: PropertyKey.summary)
+        aCoder.encode(genres, forKey: PropertyKey.genres)
+    }
+    
+    required convenience init(coder aDecoder: NSCoder){
+        let id = aDecoder.decodeInt32(forKey: PropertyKey.id)
+        let name = aDecoder.decodeObject(forKey: PropertyKey.name) as! String
+        let type = aDecoder.decodeObject(forKey: PropertyKey.type) as! String
+        let language = aDecoder.decodeObject(forKey: PropertyKey.language) as! String
+        let summary = aDecoder.decodeObject(forKey: PropertyKey.summary) as! String
+        let genres = aDecoder.decodeObject(forKey: PropertyKey.genres) as! Array<String>
+        
+        self.init(id: id, name: name, type: type, language: language, summary: summary, genres: genres)!
+        
+        
+    }
+}
--- TV TIMe/ViewController.swift
+++ TV TIMe/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import os.log
 
 class ViewController: UIViewController {
+    
+    var tvShows = [TVShow]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,97 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func getData(_ sender: Any) {
+        saveShows()
+        }
+    
+    @IBAction func showData(_ sender: Any) {
+        
+        if let savedShows = loadShows(){
+            tvShows += savedShows
+        }
+        for show in tvShows {
+            print (show.name)
+            
+        }
+        print(tvShows.count)
+    }
+    
+    
+    func saveShows() {
+        print("action works")
+        
+        
+        
+        let page = 1
+        let webpage = "https://api.tvmaze.com/shows?page=\(page)"
+        let url: NSURL = NSURL(string: webpage)!
+        
+        /*Alamofire.request("https://api.tvmaze.com/shows?page=0").responseJSON { (responseData) -> Void in
+         if((responseData.result.value) != nil) {
+         let swiftyJsonVar = JSON(responseData.result.value!)
+         //print(swiftyJsonVar)
+         for item in swiftyJsonVar.array! {
+         let show = TVShow(*/
+        
+        /*print(item["name"].stringValue)
+         print(item["genres"].arrayValue)
+         print(Int32(item["id"].intValue))
+         print(item["type"].stringValue)
+         print(item["language"].stringValue)
+         print(item["summary"].stringValue)*/
+        
+        
+        /*URLSession.shared.dataTask(with: url!) { (data, response, error) in
+         
+         if error != nil {
+         print("There was an issue")
+         print(error)
+         } else {
+         
+         print("got to here")
+         do {
+         print("Json work")
+         
+         self.json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: AnyObject]
+         let myData = self.json["data"]!*/
+        do {
+            let data: Data = try! Data(contentsOf: url as URL)
+            let jsonArray = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! NSMutableArray
+            
+            for jsonObject in jsonArray! {
+                if let show = TVShow(json: (jsonObject as AnyObject) as! [String : Any]) {
+                    tvShows.append(show)
+                    
+                    
+                }
+                if tvShows.count == 0 {
+                    print("no shows parsed")
+                }
+                
+            }
+            print(tvShows.count)
+            print(tvShows[45].name)
+            print(tvShows[226].genres)
+            print(tvShows[137].summary)
+        }
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(tvShows, toFile: TVShow.ArchiveURL.path)
+        
+        if isSuccessfulSave {
+            os_log("Show saved", log: OSLog.default, type: .debug)
+        } else {
+            os_log("Failed", log: OSLog.default, type: .error)
+        }
+    }
+    
+    private func loadShows() -> [TVShow]? {
+        
+        return NSKeyedUnarchiver.unarchiveObject(withFile: TVShow.ArchiveURL.path) as? [TVShow]!
+        
+    }
+    
+    }
+    
 
-}
+
 
